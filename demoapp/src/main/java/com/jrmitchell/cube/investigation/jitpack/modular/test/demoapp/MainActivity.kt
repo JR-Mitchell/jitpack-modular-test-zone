c4fc54c6a7e582b18a72cd979b840fd17dd05701
JR-Mitchell <www.aereregion.webs.com@live.co.uk>
2021-12-16 16:16:40 +0000

    Implement demo MainActivity.kt

---
+++ demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jrmitchell.cube.investigation.jitpack.modular.test.demoapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.jrmitchell.cube.investigation.jitpack.modular.test.android.activity.DefaultActivityDisplayTarget
+import com.jrmitchell.cube.investigation.jitpack.modular.test.demoapp.databinding.ActivityMainBinding
+
+class MainActivity : DefaultActivityDisplayTarget<PlaceholderImageLoader>() {
+	lateinit var binding : ActivityMainBinding
+	override val buttonView: TextView get() = binding.display.findViewById(R.id.display_button)
+	override val imageLoader = PlaceholderImageLoader()
+	override val imageView: ImageView get() = binding.display.findViewById(R.id.display_image)
+	override val loadingUi: View get() = binding.loading
+	override val titleView: TextView get() = binding.display.findViewById(R.id.display_title)
+	
+	val populator = PlaceholderDisplayPopulator()
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		
+		populator.populateDisplayFromUri(null, this)
+	}
+	
+}
