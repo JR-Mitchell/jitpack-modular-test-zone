8aad9b79d916948447769a16a59828aa634331ea
JR-Mitchell <www.aereregion.webs.com@live.co.uk>
2021-12-16 12:31:25 +0000

    Add core data classes

---
+++ core/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/core/data/ImageData.kt
@@ -0,0 +1,10 @@
+package com.jrmitchell.cube.investigation.jitpack.modular.test.core.data
+
+/**
+ * Data class representing an image
+ *
+ * @param width the width, in DP, of the image
+ * @param height the height, in DP, of the image
+ * @param uri a URI to the image
+ */
+data class ImageData(val width : Int, val height : Int, val uri : String?)
