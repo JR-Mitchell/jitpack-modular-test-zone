2c1a73f255c9bce9fa7fa534fd6aabb53cda5478
JR-Mitchell <www.aereregion.webs.com@live.co.uk>
2021-12-17 09:44:31 +0000

    Update DisplayData to use ButtonData, and AndroidDisplayTarget.kt to reflect this update

--- android/core/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/android/core/abstract/AndroidDisplayTarget.kt
+++ android/core/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/android/core/abstract/AndroidDisplayTarget.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import com.jrmitchell.cube.investigation.jitpack.modular.test.core.abstract.DisplayTarget
+import com.jrmitchell.cube.investigation.jitpack.modular.test.core.data.ButtonData
 import com.jrmitchell.cube.investigation.jitpack.modular.test.core.data.DisplayData
 
 /**
@@ -14,40 +15,37 @@ interface AndroidDisplayTarget<R : AndroidActionResolver, L : AndroidImageLoader
 	/**
 	 * View to be used as loading UI
 	 */
-	val loadingUi : View
+	val loadingUi: View
 	
 	/**
 	 * View to display the data's main image in
 	 */
-	val imageView : ImageView
+	val imageView: ImageView
 	
 	/**
 	 * View to display the data's title in
 	 */
-	val titleView : TextView
+	val titleView: TextView
 	
 	/**
-	 * View to display the data's button in
+	 * View to display the data's top button in
 	 */
-	val buttonView : TextView
+	val topButtonView: TextView
 	
 	/**
 	 * Resolver for clicks on the button
 	 */
-	val actionResolver : R
+	val actionResolver: R
 	
 	/**
 	 * Loader for the image
 	 */
-	val imageLoader : L
+	val imageLoader: L
 	
 	override fun displayData(data: DisplayData) {
 		setLoadingState(false)
 		titleView.text = data.titleText
-		buttonView.text = data.buttonText
-		buttonView.setOnClickListener {
-			actionResolver.resolveAction(it.context, data.buttonAction)
-		}
+		populateButton(topButtonView, data.topButton)
 		imageView.contentDescription = data.imageContentDescription
 		imageLoader.loadImage(data.imageData, imageView)
 	}
@@ -58,5 +56,12 @@ interface AndroidDisplayTarget<R : AndroidActionResolver, L : AndroidImageLoader
 	
 	override fun setLoadingState(isLoading: Boolean) {
 		loadingUi.isVisible = isLoading
+	}
+	
+	private fun populateButton(button: TextView, data: ButtonData) {
+		button.text = data.buttonText
+		button.setOnClickListener {
+			actionResolver.resolveAction(it.context, data.buttonAction)
+		}
 	}
 }
--- core/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/core/data/DisplayData.kt
+++ core/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/core/data/DisplayData.kt
@@ -6,7 +6,6 @@ package com.jrmitchell.cube.investigation.jitpack.modular.test.core.data
  * @param imageData the image to display
  * @param imageContentDescription the content description for the image
  * @param titleText the title to display
- * @param buttonText the text to display on the button
- * @param buttonAction the action to display on the button
+ * @param topButton the data to display for the top button
  */
-data class DisplayData(val imageData: ImageData, val imageContentDescription: String, val titleText: String, val buttonText: String, val buttonAction: ActionData)
+data class DisplayData(val imageData: ImageData, val imageContentDescription: String, val titleText: String, val topButton : ButtonData)
