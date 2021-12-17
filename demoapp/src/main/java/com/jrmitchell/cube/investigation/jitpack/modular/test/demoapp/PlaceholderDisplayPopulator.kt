5cc4fb1ee7aeb34bdb609dbed6dbc86c78f8d494
JR-Mitchell <www.aereregion.webs.com@live.co.uk>
2021-12-17 13:46:28 +0000

    Add further logic to PlaceholderDisplayPopulator.kt

--- demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/PlaceholderDisplayPopulator.kt
+++ demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/PlaceholderDisplayPopulator.kt
@@ -10,15 +10,69 @@ import com.jrmitchell.cube.investigation.jitpack.modular.test.core.data.ImageDat
 class PlaceholderDisplayPopulator : DisplayPopulator {
 	
 	companion object {
-		private val data = DisplayData(
+		private val defaultData = DisplayData(
 			ImageData(40, 40, "stuff"),
 			"android figure",
 			"Main Title",
-			listOf(ButtonData("Next", ActionData("activity")))
+			listOf(
+				ButtonData("Next", ActionData("activityFirst")),
+				ButtonData("Finish", ActionData("close"))
+			)
+		)
+		private val data = hashMapOf<String?, DisplayData>(
+			"activityFirst" to DisplayData(
+				ImageData(40, 40, "stuff"),
+				"android figure",
+				"First page",
+				listOf(
+					ButtonData("Option A", ActionData("activityOptionA")),
+					ButtonData("Option B", ActionData("activityOptionB")),
+					ButtonData("Option C", ActionData("activityOptionC")),
+					ButtonData("Back", ActionData("back"))
+				)
+			),
+			"activityOptionA" to DisplayData(
+				ImageData(40, 40, "stuff"),
+				"android figure",
+				"You selected Option A",
+				listOf(
+					ButtonData("Confirm", ActionData("activityConfirm")),
+					ButtonData("Back", ActionData("back")),
+					ButtonData("Close", ActionData("close"))
+				)
+			),
+			"activityOptionB" to DisplayData(
+				ImageData(40, 40, "stuff"),
+				"android figure",
+				"You selected Option B",
+				listOf(
+					ButtonData("Confirm", ActionData("activityConfirm")),
+					ButtonData("Back", ActionData("back")),
+					ButtonData("Close", ActionData("close"))
+				)
+			),
+			"activityOptionC" to DisplayData(
+				ImageData(40, 40, "stuff"),
+				"android figure",
+				"You selected Option C",
+				listOf(
+					ButtonData("Confirm", ActionData("activityConfirm")),
+					ButtonData("Back", ActionData("back")),
+					ButtonData("Close", ActionData("close"))
+				)
+			),
+			"activityConfirm" to DisplayData(
+				ImageData(40, 40, "stuff"),
+				"android figure",
+				"You confirmed",
+				listOf(
+					ButtonData("Close", ActionData("close"))
+				)
+			),
 		)
 	}
 	
 	override fun populateDisplayFromUri(pageUri: String?, target: DisplayTarget) {
-		target.displayData(data)
+		target.displayData(data[pageUri] ?: defaultData)
 	}
 }
