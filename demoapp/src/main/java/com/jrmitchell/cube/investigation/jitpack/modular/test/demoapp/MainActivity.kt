4c61e8fc4cb4c1cd88ff14d81d32da1f121746cf
JR-Mitchell <www.aereregion.webs.com@live.co.uk>
2021-12-21 14:43:37 +0000

    Update demo MainActivity to use spinner impl for determining UI type

--- demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/MainActivity.kt
+++ demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jrmitchell.cube.investigation.jitpack.modular.test.demoapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.jrmitchell.cube.investigation.jitpack.modular.test.demoapp.databinding.ActivityMainBinding
 
@@ -12,14 +13,16 @@ class MainActivity : AppCompatActivity() {
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 		
-		binding.buttonActivity.setOnClickListener {
-			startActivity(Intent(this, ActivityActivity::class.java).apply {
-				addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-			})
-		}
+		binding.uiSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, UiType.values())
 		
-		binding.buttonFragment.setOnClickListener {
-			startActivity(Intent(this, FragmentActivity::class.java))
+		binding.goButton.setOnClickListener {
+			when(binding.uiSpinner.selectedItem as UiType) {
+				UiType.ACTIVITY ->
+					startActivity(Intent(this, ActivityActivity::class.java).apply {
+						addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+					})
+				UiType.FRAGMENT -> startActivity(Intent(this, FragmentActivity::class.java))
+			}
 		}
 	}
 }
---
+++ demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/UiType.kt
@@ -0,0 +1,6 @@
+package com.jrmitchell.cube.investigation.jitpack.modular.test.demoapp
+
+enum class UiType {
+	ACTIVITY,
+	FRAGMENT
+}
