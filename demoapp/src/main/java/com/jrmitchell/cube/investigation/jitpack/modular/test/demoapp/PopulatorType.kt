a25f1836a2f67089828a9450db79d3e2032fc54f
JR-Mitchell <www.aereregion.webs.com@live.co.uk>
2021-12-22 10:48:52 +0000

    Update demo PopulatorType.kt to include memory cache

--- demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/PopulatorType.kt
+++ demoapp/src/main/java/com/jrmitchell/cube/investigation/jitpack/modular/test/demoapp/PopulatorType.kt
@@ -2,6 +2,7 @@ package com.jrmitchell.cube.investigation.jitpack.modular.test.demoapp
 
 import androidx.lifecycle.lifecycleScope
 import com.jrmitchell.cube.investigation.jitpack.modular.test.core.abstract.DisplayPopulator
+import com.jrmitchell.cube.investigation.jitpack.modular.test.core.data.DisplayData
 import com.jrmitchell.cube.investigation.jitpack.modular.test.populator.retrofit_gson.RetrofitGsonPopulator
 
 enum class PopulatorType(val activityGetter : ActivityActivity.() -> DisplayPopulator, val fragmentGetter : FragmentActivity.() -> DisplayPopulator) {
@@ -12,6 +13,10 @@ enum class PopulatorType(val activityGetter : ActivityActivity.() -> DisplayPopu
 	RETRO_GSON_LOADED(
 		{ RetrofitGsonPopulator("https://raw.githubusercontent.com/JR-Mitchell/jitpack-modular-test-zone/$CURRENT_BRANCH/demoapp/demodata/", { this.lifecycleScope }, ".json")},
 		{ RetrofitGsonPopulator("https://raw.githubusercontent.com/JR-Mitchell/jitpack-modular-test-zone/$CURRENT_BRANCH/demoapp/demodata/", { this.lifecycleScope }, ".json")}
+	),
+	RETRO_GSON_MEMORY_CACHE(
+		{ RetrofitGsonPopulator("https://raw.githubusercontent.com/JR-Mitchell/jitpack-modular-test-zone/$CURRENT_BRANCH/demoapp/demodata/", { this.lifecycleScope }, ".json", activityMemoryCache)},
+		{ RetrofitGsonPopulator("https://raw.githubusercontent.com/JR-Mitchell/jitpack-modular-test-zone/$CURRENT_BRANCH/demoapp/demodata/", { this.lifecycleScope }, ".json", hashMapOf())}
 	);
 	companion object {
 		fun valueOfOrDefault(string: String?) = try {
@@ -20,5 +25,6 @@ enum class PopulatorType(val activityGetter : ActivityActivity.() -> DisplayPopu
 			HARDCODED
 		}
 		const val CURRENT_BRANCH = "main"
+		private val activityMemoryCache = hashMapOf<String?, DisplayData>()
 	}
 }
